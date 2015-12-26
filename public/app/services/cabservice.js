d0f205ec38332aa6cf467cf9148720491974f31d
Sukalpo Mitra <sukalpomitra@gmail.com>
2015-12-26 16:18:47 +0800

    Organized the code

---
+++ public/app/services/cabservice.js
@@ -0,0 +1,66 @@
+// Handle http server errors
+app.factory('myHttpInterceptor', function ($q,toaster) {
+    return {
+        responseError: function (response) {
+          console.log(response);
+          if(response.data){
+            if(response.data.message)
+            toaster.error("Error: ", response.data.message);
+            else
+            toaster.error("Error: ", response.data);
+          }
+          return $q.reject(response);
+        }
+    };
+});
+
+// Create a resource factory to access cabs table from database 
+app.factory('Cab', function($resource) {
+  return $resource('/api/cabs/:id', { id: '@_id' }, {
+    update: { // We need to define this method manually as it is not provided with ng-resource
+      method: 'PUT'
+    }
+  });
+});
+
+app.factory('cabService', ['$http', '$q', function($http, $q){
+  var obj = {};
+  obj.carLocs = [];
+  obj.cab = {};
+  var url = 'http://jschallenge.smove.sg/provider/1/availability?';
+
+  obj.bookCab = function(start, end, cab){
+    obj.cab = cab;
+    url = url + 'book_start=' + start + '&book_end=' + end;
+    return $http.get(url)
+      .success(function(result) {
+        angular.copy(result, obj.carLocs);
+      });
+
+      return deferred.promise;
+  }
+
+  obj.sendSMS = function(){
+    var domain = "http://localhost:9000"
+    var url = domain + '/send?phone='+ obj.cab.phone+'&name='+ obj.cab.name;
+    return $http.get(url).success(function(result) {
+      });
+  }
+
+  obj.decorateMap = function(cabs)
+  {
+      var booking = [ ];
+      var cancel = [];
+      for (var i=0;i < cabs.length; i++)
+      {
+        if (cabs[i].cancelled == 0)
+          booking.push(cabs[i]);
+        else
+          cancel.push(cabs[i]);
+      }
+      
+      return [[booking.length],[booking.length + cancel.length],[cancel.length],[cabs.length + cancel.length]];
+  }
+
+  return obj;
+}]);
